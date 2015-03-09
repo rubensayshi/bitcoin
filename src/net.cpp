ae90347b484aad47258b68db664fdec2c12421b4
Ruben de Vries <ruben@rubensayshi.com>
2015-03-09 12:52:28 +0100

    changed MAX_OUTBOUND_CONNECTIONS to 100

--- src/net.cpp
+++ src/net.cpp
@@ -53,7 +53,7 @@
 using namespace std;
 
 namespace {
-    const int MAX_OUTBOUND_CONNECTIONS = 8;
+    const int MAX_OUTBOUND_CONNECTIONS = 100;
 
     struct ListenSocket {
         SOCKET socket;
